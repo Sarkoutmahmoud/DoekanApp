bbfb6663268dba6234b7876742494c9e374ab9c1
sarcode <sarkout.mahmoud@rabobank.nl>
2019-09-12 20:05:29 +0200

    Init

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  name = 'Angular';
+}
