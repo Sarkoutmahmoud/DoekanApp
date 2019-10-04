ceeacde6dda5adcf8a1085dbca249cb7ccee837e
sarcode <sarkout.mahmoud@rabobank.nl>
2019-10-04 23:09:49 +0200

    drag drop

--- src/app/main/app.component.ts
+++ src/app/main/app.component.ts
@@ -41,14 +41,7 @@ export class AppComponent {
   }
 
   drop(event: CdkDragDrop<Product[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex);
-    }
+    moveItemInArray(this.products, event.previousIndex, event.currentIndex);
   }
 
 
