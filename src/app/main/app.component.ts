71d966ea80b9428e62bdbbcb014bea45d086e4e0
sarcode <sarkout.mahmoud@rabobank.nl>
2019-10-04 22:32:50 +0200

    completed are strikethrough

---
+++ src/app/main/app.component.ts
@@ -0,0 +1,48 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {Component} from '@angular/core';
+import {Product} from "../model/Product/product";
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from "@angular/cdk/drag-drop";
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  name = 'Angular';
+
+  products : Product[]  = [{
+    quantity: 2,
+    productName: 'bananen',
+    complete: false
+  },
+    {
+      quantity: 3,
+      productName: 'apple',
+      complete: true
+    },
+    {
+      quantity: 4,
+      productName: 'peer',
+      complete: false
+    }];
+
+  newProduct: Product = new Product();
+
+  constructor() {
+  }
+
+  drop(event: CdkDragDrop<Product[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+}
